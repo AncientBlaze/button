a5f0187003d990ebb87a8527a172aad52dada957
AncientBlaze <saikata62@gmail.com>
2024-06-16 18:50:54 +0530

    road to completion

---
+++ pages/products/product_data/Ghee_data/Ghee_data.js
@@ -0,0 +1,97 @@
+export const a2 = [
+    { img: "" },
+    { img: "" },
+    { img: "" },
+    { img: "" },
+    { img: "" },
+    { img: "" },
+];
+
+export const products = [
+    {
+        ImageUrl:
+            "https://farmnaturelle.com/cdn/shop/files/ShilajitFront25g_800x.png?v=1711173556",
+        PlaceHolder:
+            "https://farmnaturelle.com/cdn/shop/files/Shilajitresin_800x.png?v=1711173556",
+        title: "Pure Himalayan Shilajit",
+        content: 429,
+        PreviousPrice: 579,
+        save: 26,
+    },
+    {
+        id: "black-sesame-oil",
+        catagory: "best-seller-cooking-oil",
+        ImageUrl:
+            "https://farmnaturelle.com/cdn/shop/files/Rectangle9313_800x.jpg?v=1708001210",
+        PlaceHolder:
+            "https://farmnaturelle.com/cdn/shop/files/Rectangle9314_800x.jpg?v=1708001212",
+        title: "Black Sesame Oil...",
+        content: 368,
+        PreviousPrice: 449,
+        save: 18,
+    },
+    {
+        ImageUrl:
+            "https://farmnaturelle.com/cdn/shop/files/Rectangle9429_800x.jpg?v=1703849984",
+        PlaceHolder:
+            "https://farmnaturelle.com/cdn/shop/files/Rectangle9430_800x.jpg?v=1703849986",
+        title: "Buy 100% Pure...",
+        content: 275,
+        PreviousPrice: 329,
+        save: 16,
+        hidden: "hidden",
+    },
+    {
+        id: "acacia-flora-honey",
+        catagory: "Honey",
+        PlaceHolder:
+            "https://farmnaturelle.com/cdn/shop/files/3_02314507-5857-4219-a1b5-306c47a83dd1_800x.jpg?v=1708000860",
+        ImageUrl:
+            "https://farmnaturelle.com/cdn/shop/files/2_58201a2c-cc05-4076-8ce1-c2ad03d1b0c4_800x.jpg?v=1708000855",
+        title: "100% Pure &...",
+        PreviousPrice: 459,
+        content: 369,
+        save: 16,
+        hidden: "hidden",
+    },
+    {
+        ImageUrl:
+            "https://farmnaturelle.com/cdn/shop/files/Rectangle9304_5e288c25-7779-4d43-b049-30fa89946889_800x.jpg?v=1708001211",
+        PlaceHolder:
+            "https://farmnaturelle.com/cdn/shop/files/Rectangle9305_444d3e7e-98ea-43f4-8c19-c4b7f178614e_800x.jpg?v=1708001213",
+        title: "Organic Cold Pressed...",
+        content: 290,
+        PreviousPrice: 339,
+        save: 14,
+    },
+    {
+        PlaceHolder:
+            "https://farmnaturelle.com/cdn/shop/files/3_04c1d709-5e90-40b5-83fc-6ee87ba55563_800x.jpg?v=1708000860",
+        ImageUrl:
+            "https://farmnaturelle.com/cdn/shop/files/2_52d72052-4078-4146-a5bb-7eb9c66b2070_800x.jpg?v=1703749282",
+        title: "Vana Tulsi Flower",
+        PreviousPrice: 459,
+        content: 369,
+        save: 16,
+    },
+    {
+        ImageUrl:
+            "https://farmnaturelle.com/cdn/shop/files/Rectangle9423_800x.jpg?v=1703749253",
+        PlaceHolder:
+            "https://farmnaturelle.com/cdn/shop/files/Rectangle9424_800x.jpg?v=1708001213",
+        title: "100% Natural Cold",
+        content: 290,
+        PreviousPrice: 339,
+        save: 14,
+    },
+    {
+        ImageUrl:
+            "https://farmnaturelle.com/cdn/shop/files/Rectangle9449_800x.jpg?v=1708001248",
+        PlaceHolder:
+            "https://farmnaturelle.com/cdn/shop/files/Rectangle9450_800x.jpg?v=1708001251",
+        title: "100% Pure Black...",
+        content: 339,
+        PreviousPrice: 430,
+        save: 21,
+    },
+];
