64a3a49ff2fa872223ffdd9d0d39071a5dd7f3ef
AncientBlaze <saikata62@gmail.com>
2024-06-17 09:17:07 +0530

    road to completion

---
+++ pages/products/product_data/Apple_data/Apple_data.js
@@ -0,0 +1,35 @@
+export const CinnamonHoney = [
+    { img: "https://farmnaturelle.com/cdn/shop/files/2_38e8be56-efc8-4584-bb40-a9998cd27a2f.jpg?v=1703749530" },
+    { img: "https://farmnaturelle.com/cdn/shop/files/3_6e7d5bcd-67a9-470c-88e9-955ce7799ee3.jpg?v=1708001287" },
+    { img: "https://farmnaturelle.com/cdn/shop/files/5_75dc0788-95fe-4c08-9872-9ad021d6c003.jpg?v=1708001290" },
+    { img: "https://farmnaturelle.com/cdn/shop/files/6_ac88cac0-7d05-4b07-bb23-8688e3dd2634.jpg?v=1703749536" },
+    { img: "https://farmnaturelle.com/cdn/shop/files/4_3644a663-21d2-423e-89c7-20a10c344e7d.jpg?v=1703749538" },
+    { img: "https://farmnaturelle.com/cdn/shop/files/1_beecb9c4-c7ab-4791-b933-629b7c2600ed.jpg?v=1708001296" },
+];
+
+export const GingerTurmeric = [
+    { img: "https://farmnaturelle.com/cdn/shop/files/2_33e16e03-4d5c-41ba-8b9c-fc9feb96421c.jpg?v=1708001285" },
+    { img: "https://farmnaturelle.com/cdn/shop/files/3_ec189c7d-a173-43ed-a806-7ef2afc5b96b.jpg?v=1708001287" },
+    { img: "https://farmnaturelle.com/cdn/shop/files/5_70e33ac1-fcd3-4165-81f0-c38170e479b5.jpg?v=1708001289" },
+    { img: "https://farmnaturelle.com/cdn/shop/files/6_b9e04528-95b5-49d4-af15-982862d8d22c.jpg?v=1703749585" },
+    { img: "https://farmnaturelle.com/cdn/shop/files/4_5f0a7d4d-967b-43e1-895a-94917abd4379.jpg?v=1703749587" },
+    { img: "https://farmnaturelle.com/cdn/shop/files/1_6a092af1-e628-4110-95e8-7419967be30c.jpg?v=1703749589" },
+];
+
+export const GingerGarlic = [
+    { img: "https://farmnaturelle.com/cdn/shop/files/2_cdab425a-39f3-4172-b187-b18e7deb07c0.jpg?v=1708001285" },
+    { img: "https://farmnaturelle.com/cdn/shop/files/3_717f62ae-178b-4bb7-a2c9-58c221c4bd43.jpg?v=1703749557" },
+    { img: "https://farmnaturelle.com/cdn/shop/files/5_d8c70ba7-a5c7-4508-b86b-1556eabb3642.jpg?v=1703749559" },
+    { img: "https://farmnaturelle.com/cdn/shop/files/6_64fcd309-2fb6-49f2-b7ff-a949398466a4.jpg?v=1703749561" },
+    { img: "https://farmnaturelle.com/cdn/shop/files/4_00dc50ff-2858-414f-83ab-dd645cc03d18.jpg?v=1708001294" },
+    { img: "https://farmnaturelle.com/cdn/shop/files/1_d3fbb482-9610-475b-b554-a255131c8c2e.jpg?v=1708001296" },
+];
+
+export const AppleCider = [
+    { img: "https://farmnaturelle.com/cdn/shop/files/2_d93ab713-7e93-4bf7-9830-34a2061f10ff.jpg?v=1703749654" },
+    { img: "https://farmnaturelle.com/cdn/shop/files/3_8ccadb4a-dd63-4a45-8279-e6c6c699a018.jpg?v=1708001287" },
+    { img: "https://farmnaturelle.com/cdn/shop/files/5_ff5fa8e1-8632-4290-b17d-13cc1bdba631.jpg?v=1708001289" },
+    { img: "https://farmnaturelle.com/cdn/shop/files/6_2791018a-3843-447e-9407-ff3e26f843ca.jpg?v=1703749661" },
+    { img: "https://farmnaturelle.com/cdn/shop/files/1_c62f746d-a782-4244-a530-e6e9df92284b.jpg?v=1703749663" },
+    { img: "https://farmnaturelle.com/cdn/shop/files/4_74e3ccee-c1b5-4969-b8e5-f1d997821979.jpg?v=1708001296" },
+];
